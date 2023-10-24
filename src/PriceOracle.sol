eb8a9eaf12071c6e7bee53b96555c59297404e77
Azhar Haque <azhar.haque.io@gmail.com>
2023-10-24 03:41:26 -0400

    updated file

--- src/PriceOracle.sol
+++ src/PriceOracle.sol
@@ -1,24 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+interface IPriceOracle {
+    function getPrice(string memory symbol) external view returns (uint256);
+}
 
-contract PriceOracle {
-    mapping(address => AggregatorV3Interface) private priceFeeds;
+contract CryptoPriceOracle is IPriceOracle {
+    mapping(string => uint256) private prices;
 
-    constructor(address[] memory _tokens, address[] memory _feeds) {
-        require(_tokens.length == _feeds.length, "Invalid input length");
-
-        for (uint256 i = 0; i < _tokens.length; i++) {
-            priceFeeds[_tokens[i]] = AggregatorV3Interface(_feeds[i]);
-        }
+    function getPrice(string memory symbol) external view override returns (uint256) {
+        return prices[symbol];
     }
 
-    function getPrice(address _token) external view returns (int256) {
-        AggregatorV3Interface priceFeed = priceFeeds[_token];
-        require(address(priceFeed) != address(0), "Invalid token");
-
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        return price;
+    function setPrice(string memory symbol, uint256 price) external {
+        prices[symbol] = price;
     }
 }
